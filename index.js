b9ea6fbd197f653bd14f3cff3f987ca402fb7eff
MickvdBoom <mickvdboom@gmail.com>
2024-06-01 14:49:57 +0200

    Laatste fixes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Dit zijn al mijn knoppen
 const Trainknop = document.querySelector("#Train")
 const Aaiknop = document.querySelector("#Aai")
 const GeefEtenKnop = document.querySelector("#GeefEten")
 const StartSpelKnop = document.querySelector("#SpelStart")
 
+// Dit zijn mijn variabele getallen
 let Fitheid = 50
 let Vrolijkheid = 50
 let Eten = 50
@@ -13,42 +15,49 @@ let AaiVeld = document.querySelector("#VrolijkheidWaarde")
 let GeefEtenVeld = document.querySelector("#EtenWaarde")
 let audio = new Audio('geluiden/rip-dave.mp3')
 
+// Deze functie groet de gebruiker
 function groet(gebruiker) {
     console.log("Hallo " + gebruiker + "! Ik ben Dave.")
 }
-
 groet("Mick")
 
+// Deze functie verhoogd de waarde van Dave zijn fitheid
 function verhoogFitheid() {
     Fitheid = Fitheid + 5
     updateFitheid()
 }
 
+// Deze functie verhoogd de waarde van Dave zijn vrolijkheid
 function verhoogVrolijkheid() {
     Vrolijkheid = Vrolijkheid + 5
     updateVrolijkheid()
 }
 
+// Deze functie verhoogd de waarde van Dave zijn eten
 function verhoogEten() {
     Eten = Eten + 5
     updateEten ()
 }
 
+// Deze functie update de waarde van Dave zijn fitheid
 function updateFitheid() {
     console.log("getal: " + Fitheid)
     TrainVeld.textContent = Fitheid
 }
 
+// Deze functie update de waarde van Dave zijn vrolijkheid
 function updateVrolijkheid() {
     console.log("getal: " + Vrolijkheid)
     AaiVeld.textContent = Vrolijkheid
 }
 
+// Deze functie update de waarde van Dave zijn eten
 function updateEten() {
     console.log("getal: " + Eten)
     GeefEtenVeld.textContent = Eten
 }
 
+// Deze functie verlaagd de waarde van Dave zijn fitheid
 function verlaagFitheid() {
     Fitheid = Fitheid - 1
     setTimeout(verlaagFitheid, 1000)
@@ -56,6 +65,7 @@ function verlaagFitheid() {
 }
 /*Bron voor code: https://stackoverflow.com/questions/7188145/call-a-javascript-function-every-5-seconds-continuously*/
 
+// Deze functie verlaagd de waarde van Dave zijn vrolijkheid
 function verlaagVrolijkheid() {
     Vrolijkheid = Vrolijkheid - 1
     setTimeout(verlaagVrolijkheid, 1000)
@@ -63,6 +73,7 @@ function verlaagVrolijkheid() {
 }
 /*Bron voor code: https://stackoverflow.com/questions/7188145/call-a-javascript-function-every-5-seconds-continuously*/
 
+// Deze function verlaagd de waarde van Dave zijn vrolijkheid
 function verlaagEten() {
     Eten = Eten - 1
     setTimeout(verlaagEten, 1000)
@@ -70,6 +81,7 @@ function verlaagEten() {
 }
 /*Bron voor code: https://stackoverflow.com/questions/7188145/call-a-javascript-function-every-5-seconds-continuously*/
 
+// Deze functie checkt de status van dave en verteld de gebruiker wat hij moet doen
 function checkStatus() {
     if (Fitheid, Vrolijkheid, Eten > 20){
     console.log("Goed bezig! Dave is blij!")
@@ -95,18 +107,17 @@ function checkStatus() {
 /*Bron voor code: https://stackoverflow.com/questions/7188145/call-a-javascript-function-every-5-seconds-continuously*/
 /*Bron voor code: https://stackoverflow.com/questions/9419263/how-to-play-audio*/
 
+
+// Deze functie laat het spel starten en verwijderd de start knop
 function startSpel() {
     verlaagFitheid ()
     verlaagVrolijkheid ()
     verlaagEten ()
     checkStatus ()
-    verberg ()
-}
-
-function verberg() {
     StartSpelKnop.remove()
 }
 
+// Dit zijn mijn knoppen
 Trainknop.addEventListener('click', verhoogFitheid)
 Aaiknop.addEventListener('click', verhoogVrolijkheid)
 GeefEtenKnop.addEventListener('click', verhoogEten)
